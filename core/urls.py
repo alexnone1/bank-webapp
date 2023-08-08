ab62b26665b353076d0fc6d19b77a28ec5e5a607
Peter Kimbeghi <peterkimbeghi@gmail.com>
2023-08-08 22:31:00 +0100

    changed a the theme and hero section on home page

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home_view, name="home_view"),
     path('dashboard/', views.dashboard_view, name="dashboard_view"),
     path('auth/register/', views.register_view, name="register_view"),
+    path("auth/login/", views.login_view, name="login_view"),
     path('auth/logout/', views.logout_view, name="logout_view"),
-    
+
 ]
--- core/views.py
+++ core/views.py
@@ -4,24 +4,31 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import AccountUsers
 
+
 def register_view(request):
-	if request.method == "POST":
-		form = AccountUsers(request.POST)
-		if form.is_valid():
-			form.save()
-			messages.success(request, "Registration successful." )
-			return redirect("home_view")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = AccountUsers()
-	return render (request=request, template_name="signup.html", context={"form":form})
+    if request.method == "POST":
+        form = AccountUsers(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful.")
+            return redirect("home_view")
+        messages.error(
+            request, "Unsuccessful registration. Invalid information.")
+    form = AccountUsers()
+    return render(request=request, template_name="signup.html", context={"form": form})
+
 
 def home_view(request):
     username = 'micheal'
 
     context = {
-         "username": username
+        "username": username
     }
 
+    return render(request, "dashboard/home.html", context)
+
+
+def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -30,22 +37,24 @@ def home_view(request):
             login(request, user)
             return redirect('dashboard_view')
         else:
-            messages.success(request, ("Your password is incorrect, Kindly check the password and try again!"))
-            return redirect('home_view')
+            messages.success(
+                request, ("Your password is incorrect, Kindly check the password and try again!"))
+            return redirect('login_view')
     else:
-        return render(request, "dashboard/home.html", context)
+        return render(request, "dashboard/login.html")
+
 
 def logout_view(request):
-     logout(request)
-     return redirect('home_view')
+    logout(request)
+    return redirect('home_view')
+
 
 def dashboard_view(request):
     username = "Donna Smith"
     account_balance = "$760,000"
 
     context = {
-         "username": username,
-         "account_balance": account_balance
+        "username": username,
+        "account_balance": account_balance
     }
     return render(request, "dashboard/dashboard.html", context)
-
